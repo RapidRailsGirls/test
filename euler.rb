09e401ad81bc5e0f7abaa08c19463586d66b989d
BriocheBerlin <bri.ma@gmx.de>
2014-07-22 20:03:09 +0200

    refactored 1.Euler challenge using enumerable

--- euler.rb
+++ euler.rb
@@ -14,17 +14,8 @@ attr_accessor :number_of_legs
     array.inject(:+)
   end
 
+
   def self.sum_multiples_below(threshold)
-    sum = 0
-    (1...threshold).each do |number|
-     sum += number if is_multiple_of_3_or_5?(number)
-    end
-    sum
-  end
-
-  private
-
-  def self.is_multiple_of_3_or_5?(number)
-    number % 3 == 0 or number % 5 == 0
+    (1...threshold).select{|x| x % 3 == 0 or x % 5 == 0}.inject(:+)
   end
 end
